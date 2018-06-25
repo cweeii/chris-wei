f374093546eb0a2a276c7a2027cc654687f51c38
Chris Wei <chrisz.wei@gmail.com>
2018-06-24 23:43:40 -0700

    added compression middleware

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
 const helmet = require('helmet');
+const compression = require('compression');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(helmet());
+app.use(compression());
 app.use(express.static('dist'));
 
 app.get('/*', (req, res) => {
