89b9d2b5b2ab5808f877f8879a8238a74b574c92
Chris Wei <chrisz.wei@gmail.com>
2018-06-25 00:07:30 -0700

    removed fragment wrapper

--- src/content/Info/Info.js
+++ src/content/Info/Info.js
@@ -23,51 +23,49 @@ const Info = () => {
   const time = new Date().getTime();
 
   return (
-    <React.Fragment>
-      <div className={container}>
-        <span className={name}>christopher wei</span>
-        <span className={description}>
-          I like javascript, video games, and cats
-        </span>
-        <span className={svgLinks}>
-          <Link
-            className={githubLink}
-            href={`https://github.com/cweeii?time=${time}`}
-            target="_blank"
-          >
-            <Github />
-          </Link>
-          <Link
-            className={facebookLink}
-            href={`https://www.facebook.com/cweeii?time=${time}`}
-            target="_blank"
-          >
-            <Facebook />
-          </Link>
-          <Link
-            className={twitterLink}
-            href={`https://twitter.com/cweeii?time=${time}`}
-            target="_blank"
-          >
-            <Twitter />
-          </Link>
-          <Link
-            className={linkedInLink}
-            href={`https://www.linkedin.com/in/christopher-wei-58660b69?time=${time}`}
-            target="_blank"
-          >
-            <LinkedIn />
-          </Link>
-          <Link
-            className={resumeLink}
-            href={`https://storage.googleapis.com/chris-wei-207401.appspot.com/Chris%20Wei%20Resume%CC%81.pdf?time=${time}`}
-            target="_blank"
-          >
-            <Resume />
-          </Link>
-        </span>
-      </div>
-    </React.Fragment>
+    <div className={container}>
+      <span className={name}>christopher wei</span>
+      <span className={description}>
+        I like javascript, video games, and cats
+      </span>
+      <span className={svgLinks}>
+        <Link
+          className={githubLink}
+          href={`https://github.com/cweeii?time=${time}`}
+          target="_blank"
+        >
+          <Github />
+        </Link>
+        <Link
+          className={facebookLink}
+          href={`https://www.facebook.com/cweeii?time=${time}`}
+          target="_blank"
+        >
+          <Facebook />
+        </Link>
+        <Link
+          className={twitterLink}
+          href={`https://twitter.com/cweeii?time=${time}`}
+          target="_blank"
+        >
+          <Twitter />
+        </Link>
+        <Link
+          className={linkedInLink}
+          href={`https://www.linkedin.com/in/christopher-wei-58660b69?time=${time}`}
+          target="_blank"
+        >
+          <LinkedIn />
+        </Link>
+        <Link
+          className={resumeLink}
+          href={`https://storage.googleapis.com/chris-wei-207401.appspot.com/Chris%20Wei%20Resume%CC%81.pdf?time=${time}`}
+          target="_blank"
+        >
+          <Resume />
+        </Link>
+      </span>
+    </div>
   );
 };
 
